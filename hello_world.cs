d913cf4e75ebfef521d16eba0220287ebb3261b9
gcurco <guillem.curco@gmail.com>
2020-11-15 18:29:09 +0100

    hello world

---
+++ hello_world.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld_GuillemCurco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World CS Guillem Curcó");
+        }
+    }
+}
